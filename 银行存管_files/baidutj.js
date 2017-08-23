cb1caf00a2e5396bea24805e6263ea2660e1406d
jianfei <wujianfei1991@gmail.com>
2017-08-23 18:40:30 +0800

    first commit

---
+++ 银行存管_files/baidutj.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/24.
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?c7796d9350739c79516c826b533d03e3";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
